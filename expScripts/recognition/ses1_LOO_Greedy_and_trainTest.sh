4ce1c203f767f5083bf9eb01431078123f1326fa
KailongPeng <kailongpeng001@gmail.com>
2021-06-15 16:44:30 -0400

    conda

--- expScripts/recognition/ses1_LOO_Greedy_and_trainTest.sh
+++ expScripts/recognition/ses1_LOO_Greedy_and_trainTest.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
+#SBATCH --job-name ses1_LOO_Greedy_and_trainTest
 #SBATCH --output=logs/ses1_LOO_Greedy_and_trainTest-%j.out
-##SBATCH -p day
-##SBATCH -t 24:00:00
 #SBATCH --partition=psych_day,psych_scavenge,psych_week,day,scavenge_all,week
 #SBATCH --time=6:00:00 #20:00:00
 #SBATCH --mem 4GB
 #SBATCH -n 1
 module load FSL
 # module load miniconda
+. /gpfs/milgram/apps/hpc.rhel7/software/Python/Anaconda3/etc/profile.d/conda.sh
 conda activate /gpfs/milgram/project/turk-browne/users/kp578/CONDA/rtcloud
 
 toml=$1 # sub002.ses5.toml
